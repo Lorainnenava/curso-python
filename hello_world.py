65e0dc5514985c4fb0cab18d3c86fdc3c4510a13
LorainneNavarro <lnavarro@wowdesarrollos.com>
2024-05-31 17:29:02 -0500

    curso python

---
+++ hello_world.py
@@ -0,0 +1,35 @@
+print("hello world")
+
+name= "Lorainne"
+
+if name == "Lorainne":
+    print(name) #Lorainne
+
+# --- ESPACIADOS ---
+y = 1  +  2
+print(y) #3
+
+# incorrecto
+x = 1 
++ 2
+print(x) #1
+
+f= 1 \
++ 2
+print(f) #3
+
+# SANGRIA
+def say_hello():
+    print("Hello there!")
+
+print(say_hello())
+
+myName= "Lorainne"
+print(type(myName))
+
+del myName # Elimina la variable
+print(myName)
+
+a= "hola"
+b= "como estas"
+print(a + b)
